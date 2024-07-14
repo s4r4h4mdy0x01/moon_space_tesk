247d9279e3f5bbf0f9ceea10723e85e97dae25f2
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2024-07-14 06:23:19 +0300

    Change Favorite Icon when  press it

--- lib/features/home/ui/widgets/stack_image_and_favorite_icon.dart
+++ lib/features/home/ui/widgets/stack_image_and_favorite_icon.dart
@@ -3,46 +3,63 @@ import 'package:flutter/material.dart';
 
 import 'package:moon_space_task/core/theming/colors.dart';
 
-class StackImageAndFavoriteIcon extends StatelessWidget {
+class StackImageAndFavoriteIcon extends StatefulWidget {
   final String image;
+
   const StackImageAndFavoriteIcon({super.key, required this.image});
- //This widget contains the Stack contains A  Image with FavoriteIcon a  above it is displayed in the ProductsCard Widget
+
+  @override
+  _StackImageAndFavoriteIconState createState() => _StackImageAndFavoriteIconState();
+}
+
+class _StackImageAndFavoriteIconState extends State<StackImageAndFavoriteIcon> {
+  bool _isFavorite = false;
+
+  void _toggleFavorite() {
+    setState(() {
+      _isFavorite = !_isFavorite;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Container(
-          //       height:100.h,
-          decoration:   BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(14),
-                topRight: Radius.circular(14),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.shade400,
-                  offset: const  Offset(1, 2),
-                  blurRadius: 1,
-                )
-              ]),
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(14),
+              topRight: Radius.circular(14),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade400,
+                offset: const Offset(1, 2),
+                blurRadius: 1,
+              )
+            ],
+          ),
           child: CachedNetworkImage(
-            imageUrl: image,
+            imageUrl: widget.image,
             width: double.infinity,
             fit: BoxFit.cover,
           ),
         ),
-       Positioned(
+        Positioned(
           top: 8.0,
           right: 8.0,
-          child:CircleAvatar(
-        backgroundColor: Colors.white,
-        child: Icon(
-          Icons.favorite_border_outlined ,
-            color: ColorManager.primary),
-      ),
+          child: GestureDetector(
+            onTap: _toggleFavorite,
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Icon(
+                _isFavorite ? Icons.favorite : Icons.favorite_border_outlined,
+                color:  ColorManager.primary,
+              ),
+            ),
+          ),
         ),
       ],
     );
   }
 }
-
